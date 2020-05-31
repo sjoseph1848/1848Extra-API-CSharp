64424a366b309e8cfb4eb0adc245d4d79704e771
sjoseph1848 <sjoseph1848@gmail.com>
2020-05-31 13:51:25 -0500

    added new model for table

---
+++ API/Data/Models/PresPolls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data.Models
+{
+    public class PresPolls
+    {
+        public PresPolls()
+        {
+
+        }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// Question Id (foreign key)
+        /// </summary>
+        public int QuestionId { get; set; }
+
+        public string Stage { get; set; }
+        public int RaceId { get; set; }
+        public string? State { get; set; }
+        public string CandidateName { get; set; }
+        public string CandidateParty { get; set; }
+        public int pct { get; set; }
+
+    }
+}
